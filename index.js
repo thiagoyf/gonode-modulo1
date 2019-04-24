a46913b30e78628f9006e3ee16fb0efc5b438936
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-04-23 22:05:09 -0400

    Adding template and passing param to the template

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 
+// Nunjucks é uma template engine, ou seja é uma forma de renderizar HTML com
+// conteúdo também em javascript
 const app = express();
 
+// https://github.com/mozilla/nunjucks
+nunjucks.configure("views", {
+  // para manipular o nome dos arquivos
+  autoescape: true,
+  // variavel que vai receber nosso servidor express
+  express: app,
+  // como fosse nodemon nunjucks
+  watch: true
+});
+
+// Setar configurações globais
+app.set("view engine", "njk");
+
 app.get("/", (req, res) => {
-  return res.send(`Bem-Vindo`);
+  // primeiro parametro seria o nome do template sem a extensão
+  return res.render("list", { name: "Thiago" });
 });
 
 app.listen(3000);
