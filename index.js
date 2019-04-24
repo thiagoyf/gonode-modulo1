ccc410d5b102b164a4ae0878a18d5d13be5402b2
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-04-24 13:11:26 -0400

    Adding eslint

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
+const express = require('express')
+const nunjucks = require('nunjucks')
 
 // Nunjucks é uma template engine, ou seja é uma forma de renderizar HTML com
 // conteúdo também em javascript
-const app = express();
+const app = express()
 
 // https://github.com/mozilla/nunjucks
-nunjucks.configure("views", {
+nunjucks.configure('views', {
   // para manipular o nome dos arquivos
   autoescape: true,
   // variavel que vai receber nosso servidor express
   express: app,
   // como fosse nodemon nunjucks
   watch: true
-});
+})
 
 // para o express saber lidar com um formulário HTML
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 
 // Setar configurações globais
-app.set("view engine", "njk");
+app.set('view engine', 'njk')
 
-const users = ["Diego Fernandes", "Robson Marques", "Cleison Souza"];
+const users = ['Diego Fernandes', 'Robson Marques', 'Cleison Souza']
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   // primeiro parametro seria o nome do template sem a extensão
   // o segundo seria o nome da variável que está passando para o template
-  return res.render("list", { users });
-});
+  return res.render('list', { users })
+})
 
-app.get("/new", (req, res) => {
-  return res.render("new");
-});
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
 
 // o post é o metodo que a rota está ouvindo.
-app.post("/create", (req, res) => {
-  users.push(req.body.user);
-  return res.redirect("/");
-});
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  return res.redirect('/')
+})
 
-app.listen(3000);
+app.listen(3000)
--- index_part1.js
+++ index_part1.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express')
 
 // Express é um micro framework que lida diretamente com rotas e com a parte de
 // views
-const app = express();
+const app = express()
 
 // app.get("/", (req, res) => {
 //   return res.send(`Bem-Vindo,  ${req.query.name}`);
@@ -18,39 +18,39 @@ const app = express();
 const logMiddleware = (req, res, next) => {
   console.log(
     `HOST : ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
-  );
+  )
 
   // Modifica a requisição e passa para outros middlewares que vem após ele.
-  req.appName = "GoNode";
+  req.appName = 'GoNode'
 
   // Deixa o fluxo correr normalmente
-  return next();
-};
+  return next()
+}
 
 // Outro jeito de utilizarum middleware é utilizar o app.use(middleware)
 // Desse jeito todas as requisições vão utilizar o middleware.
-app.use(logMiddleware);
+app.use(logMiddleware)
 
 // Para lidar com rotas podemos fazer o seguinte abaixo
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   // com http, so tinhamos res.end
   // agora da para se usar res.send e res.json (resposta em json)
 
   // Esse é um query param, são parametro que vem depois do '?'
   // Exemplo: '/?name=Thiago' o nome do parametro é name, e o valor é Thiago.
-  return res.send(`Bem-Vindo ao ${req.appName},  ${req.query.name}`);
-});
+  return res.send(`Bem-Vindo ao ${req.appName},  ${req.query.name}`)
+})
 
 // URL quem vem como parametro o nome do usuário
 // para pegar o parametro na url, usa req.params.<nome_do_parametro>
 // Não é um query param
-app.get("/nome/:name", (req, res) => {
+app.get('/nome/:name', (req, res) => {
   // retorna resposta no formato JSON
   return res.json({
     message: `Bem vindo, ${req.params.name}`
-  });
-});
+  })
+})
 
 // Sobe o servidor na porta 3000
 // Importante, o listen sempre tem que ser a última coisa que vir
-app.listen(3000);
+app.listen(3000)
