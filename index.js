ca034018a75791522473a1335a6d6c5795f0f4cb
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-04-23 21:26:50 -0400

    Adding middleware that is used on all requests

--- index.js
+++ index.js
@@ -4,6 +4,33 @@ const express = require("express");
 // views
 const app = express();
 
+// app.get("/", (req, res) => {
+//   return res.send(`Bem-Vindo,  ${req.query.name}`);
+// });
+
+// '(req, res) => {}' é um middleware, ou interceptador de requisições no Node.
+// o Middleware ele pode interceptar e devolver uma resposta ou apenas fazer
+// algo na aplicação.
+
+// os middleware podem bloquear o fluxo das requisições dentro do express
+// ou não, passando a função next após o res
+
+const logMiddleware = (req, res, next) => {
+  console.log(
+    `HOST : ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
+  );
+
+  // Modifica a requisição e passa para outros middlewares que vem após ele.
+  req.appName = "GoNode";
+
+  // Deixa o fluxo correr normalmente
+  return next();
+};
+
+// Outro jeito de utilizarum middleware é utilizar o app.use(middleware)
+// Desse jeito todas as requisições vão utilizar o middleware.
+app.use(logMiddleware);
+
 // Para lidar com rotas podemos fazer o seguinte abaixo
 app.get("/", (req, res) => {
   // com http, so tinhamos res.end
@@ -11,7 +38,7 @@ app.get("/", (req, res) => {
 
   // Esse é um query param, são parametro que vem depois do '?'
   // Exemplo: '/?name=Thiago' o nome do parametro é name, e o valor é Thiago.
-  return res.send(`Bem-Vindo,  ${req.query.name}`);
+  return res.send(`Bem-Vindo ao ${req.appName},  ${req.query.name}`);
 });
 
 // URL quem vem como parametro o nome do usuário
